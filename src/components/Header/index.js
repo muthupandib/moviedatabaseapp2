6fb2bfc8200410afc56cd985e581909af44f1d53
muthupandib <muthupandib0067@gmail.com>
2024-11-19 09:47:50 +0000

    moviedatabase3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,48 +17,48 @@ const Header = () => {
   }
   return (
     <>
-      <nav className="nav-bar">
-        <Link to="/" className="link">
-          <h1 className="logo-name">
-            Prime <span className="logo-high">Show</span>
+      <nav className='nav-bar'>
+        <Link to='/' className='link'>
+          <h1 className='logo-name'>
+            Prime <span className='logo-high'>Show</span>
           </h1>
         </Link>
-        <div className="nav-items-lg">
-          <Link to="/" className="link">
-            <p className="item-lg">Home</p>
+        <div className='nav-items-lg'>
+          <Link to='/' className='link'>
+            <p className='item-lg'>Home</p>
           </Link>
-          <Link to="/top-rated" className="link">
-            <p className="item-lg">Top Rated</p>
+          <Link to='/top-rated' className='link'>
+            <p className='item-lg'>Top Rated</p>
           </Link>
-          <Link to="/upcoming" className="link">
-            <p className="item-lg">Upcoming</p>
+          <Link to='/upcoming' className='link'>
+            <p className='item-lg'>Upcoming</p>
           </Link>
         </div>
         {!isSowMenu ? (
-          <button type="button" className="menu-btn" onClick={toggleMenuBtn}>
-            <IoMdMenu className="icon" />
+          <button type='button' className='menu-btn' onClick={toggleMenuBtn}>
+            <IoMdMenu className='icon' />
           </button>
         ) : (
-          <button type="button" className="menu-btn" onClick={toggleMenuBtn}>
-            <IoMdClose className="icon" />
+          <button type='button' className='menu-btn' onClick={toggleMenuBtn}>
+            <IoMdClose className='icon' />
           </button>
         )}
       </nav>
 
       {isSowMenu ? (
-        <div className="menu-div">
-          <div className="nav-items-sm">
-            <Link to="/" className="route-link">
-              <MdHome className="route-icon" />
-              <p className="item"> Home</p>
+        <div className='menu-div'>
+          <div className='nav-items-sm'>
+            <Link to='/' className='route-link'>
+              <MdHome className='route-icon' />
+              <p className='item'> Home</p>
             </Link>
-            <Link to="/top-rated" className="route-link">
-              <RxArrowTopRight className="route-icon" />
-              <p className="item">Top Rated Movies</p>
+            <Link to='/top-rated' className='route-link'>
+              <RxArrowTopRight className='route-icon' />
+              <p className='item'>Top Rated Movies</p>
             </Link>
-            <Link to="/upcoming" className="route-link">
-              <MdSchedule className="route-icon" />
-              <p className="item">Upcoming Movies</p>
+            <Link to='/upcoming' className='route-link'>
+              <MdSchedule className='route-icon' />
+              <p className='item'>Upcoming Movies</p>
             </Link>
           </div>
         </div>
